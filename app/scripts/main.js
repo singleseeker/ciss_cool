31af5b33799029d44af9e3dfdf7afdcfa673412a
SingleSeeker <671826@qq.com>
2015-05-05 23:01:56 +0800

    Fix user html page effect.

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -11,4 +11,16 @@ $(function(){
 		window.location.href = $(this).find('a').attr('href');
 	});
 
+	$('.user-dash-nav li').not('.current').hover(function(){
+		$(this).addClass('current');
+		$(this).css('border-right','1px solid #ccc');
+		$(this).data('origin_url',$(this).find('img').attr('src'));
+		$(this).find('img').attr('src', $(this).find('img').attr('hover'));
+	} ,function(){
+		$(this).removeClass('current');
+		$(this).find('img').attr('src', $(this).data('origin_url'));
+	}).click(function(){
+		window.location.href = $(this).find('a').attr('href');
+	});
+
 });
