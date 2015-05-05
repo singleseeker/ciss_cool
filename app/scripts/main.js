dcad1138177a7e11cedd5917ca46507a95d852a3
SingleSeeker <671826@qq.com>
2015-05-05 21:28:34 +0800

    Fix about us style.

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,14 @@
-console.log('\'Allo \'Allo!');
+$(function(){
+
+	$('.single-page-nav li').not('.current').hover(function(){
+		$(this).addClass('current');
+		$(this).data('origin_url',$(this).find('img').attr('src'));
+		$(this).find('img').attr('src', $(this).find('img').attr('hover'));
+	} ,function(){
+		$(this).removeClass('current');
+		$(this).find('img').attr('src', $(this).data('origin_url'));
+	}).click(function(){
+		window.location.href = $(this).find('a').attr('href');
+	});
+
+});
